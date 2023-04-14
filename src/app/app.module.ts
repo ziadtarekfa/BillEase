03b79e1e02ccb26c8199592a17a25411206c51f0
Ziad Tarek <ziadtarekfa@gmail.com>
2023-04-14 06:42:31 +0200

    implemented routing between sign in and sign up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    SignUpComponent
+    SignUpComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', component: DashboardComponent },
+      { path: 'signin', component: SignInComponent },
+      { path: 'signup', component: SignUpComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
