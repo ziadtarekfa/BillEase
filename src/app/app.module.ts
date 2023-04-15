71f254b157b20a566c41eb1f3911087bf9daf40f
Hazem Muhammad Abdelfattah <hazem.muhammad.96@gmail.com>
2023-04-15 05:35:24 +0200

    navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,43 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppComponent } from './app.component';
-import { SignInComponent } from './sign-in/sign-in.component';
-import { SignUpComponent } from './sign-up/sign-up.component';
-import { RouterModule } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { AppComponent } from "./app.component";
+import { SignInComponent } from "./sign-in/sign-in.component";
+import { SignUpComponent } from "./sign-up/sign-up.component";
+import { RouterModule } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NavbarComponent } from "./components/layout/navbar/navbar.component";
+import { LayoutWrapperComponent } from "./components/layout/layout-wrapper/layout-wrapper.component";
+import { NavItemComponent } from "./components/layout/navbar/nav-item/nav-item.component";
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import {NgOptimizedImage} from "@angular/common";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    SignInComponent,
-    SignUpComponent,
-    DashboardComponent
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule.forRoot([
-      { path: '', component: DashboardComponent },
-      { path: 'signin', component: SignInComponent },
-      { path: 'signup', component: SignUpComponent }
-    ]),
-    FormsModule,
-    ReactiveFormsModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        SignInComponent,
+        SignUpComponent,
+        DashboardComponent,
+        NavbarComponent,
+        LayoutWrapperComponent,
+        NavItemComponent,
+    ],
+    imports: [
+        BrowserModule,
+        RouterModule.forRoot([
+            { path: "", component: DashboardComponent },
+            { path: "signin", component: SignInComponent },
+            { path: "signup", component: SignUpComponent },
+        ]),
+        FormsModule,
+        ReactiveFormsModule,
+        BrowserAnimationsModule,
+        AngularSvgIconModule.forRoot(),
+        NgOptimizedImage,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
