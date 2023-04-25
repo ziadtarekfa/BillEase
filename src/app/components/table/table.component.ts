29e1bb4314b76ce25653447616863897a54b5c2c
Hazem Muhammad Abdelfattah <hazem.muhammad.96@gmail.com>
2023-04-26 00:55:38 +0200

    pagination ui

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -10,22 +10,31 @@ export class TableComponent implements OnInit {
     @Input() columns: TableColumns = [];
     @Input() data: TableData = [];
     actualData: TableData = this.data;
-    @Input() placeholder = "Search";
+
     @Input() itemsTitle = "Items";
     @Input() itemTitle = "Item";
+
+    @Input() placeholder = "Search";
     search: string = "";
+
+    page: number = 1;
+    totalPages: number = 1;
 
     ngOnInit(): void {
         this.actualData = this.data;
     }
 
     onFilter() {
-            this.actualData = this.data.filter((item) => {
-                return Object.values(item).some((value) => {
-                    return String(value)
-                        .toLowerCase()
-                        .includes(this.search.toLowerCase());
-                });
+        this.actualData = this.data.filter((item) => {
+            return Object.values(item).some((value) => {
+                return String(value)
+                    .toLowerCase()
+                    .includes(this.search.toLowerCase());
             });
+        });
     }
+
+    onPrev() {}
+
+    onNext() {}
 }
