00ca590d72ed2c5bfe2de106a0d4e56232a168a9
Ziad Tarek <ziadtarekfa@gmail.com>
2023-04-30 20:32:53 +0300

    added validation to payment form

--- src/app/features/user/bills/payment/payment.component.ts
+++ src/app/features/user/bills/payment/payment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnChanges, SimpleChanges } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import BillsService from "../_common/services/bills.service";
 import Bill from "../_common/services/bill";
 import { ActivatedRoute, Router } from "@angular/router";
@@ -12,17 +13,18 @@ import { AuthService } from "../../../_global/auth/_common/services/auth.service
     styleUrls: ["./payment.component.css"],
 })
 export class PaymentComponent {
+    paymentForm: FormGroup;
     bill?: Bill;
     user?: User;
     id: string;
     successfulPayment$ = new BehaviorSubject(false);
     loading = false;
-
     constructor(
         private readonly billsService: BillsService,
         private readonly authService: AuthService,
         private readonly router: Router,
-        private readonly route: ActivatedRoute
+        private readonly route: ActivatedRoute,
+        private fb: FormBuilder
     ) {
         this.id = (this.route.params as BehaviorSubject<any>).getValue().id;
         this.loading = true;
@@ -34,6 +36,21 @@ export class PaymentComponent {
                 setTimeout(() => this.router.navigate([newRoute]), 2000);
             }
         });
+        this.paymentForm = this.fb.group({
+            cardHolderName: ["", [Validators.required]],
+            cardNumber: ["",
+                [
+                    // should start with 4 or 5 and can be 13 digits or 16 digits
+                    Validators.required,
+                    Validators.pattern(
+                        "^[4-5][0-9]{12}(?:[0-9]{3})?$"
+                    ),
+                ],
+            ],
+            expiryDate: ["", [Validators.required]],
+            // should be 3 or 4 digits
+            cvv: ["", [Validators.required, Validators.pattern('^[0-9]{3,4}$')]],
+        });
     }
 
     async fetchBill() {
@@ -44,6 +61,7 @@ export class PaymentComponent {
     }
 
     async confirmPayment() {
-        this.successfulPayment$.next(await this.billsService.pay(this.id));
+        console.log(this.paymentForm);
+        // this.successfulPayment$.next(await this.billsService.pay(this.id));
     }
 }
